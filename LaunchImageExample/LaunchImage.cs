d4cc60e932420da2728bf775641401853323c319
Tom <tom@tom.me.uk>
2014-01-18 16:26:37 +0000

    Added Launch Image example project

---
+++ LaunchImageExample/LaunchImage.cs
@@ -0,0 +1,58 @@
+﻿#if LAUNCHIMAGE
+
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+using System.Threading.Tasks;
+
+public static class LaunchImage
+{
+	public static async void SayCheese()
+	{
+		HideStatusBar();
+
+		var window = UIApplication.SharedApplication.KeyWindow;
+
+		while (true)
+		{
+			HideAllTextViews(window);
+			ClearAllTitles(window.RootViewController);
+			await Task.Delay(3000);
+		}
+	}
+
+	private static void HideStatusBar()
+	{
+		UIView statusBar = (UIView)UIApplication.SharedApplication.ValueForKey(new NSString("statusBar"));
+		UIView foregroundView = (UIView)statusBar.ValueForKey(new NSString("foregroundView"));
+		foregroundView.Hidden = true;
+	}
+
+	private static void HideAllTextViews(UIView views)
+	{
+		foreach (var subView in views.Subviews)
+		{
+			if (subView.GetType().GetProperty("Text") != null)
+			{
+				subView.Hidden = true;
+			}
+
+			HideAllTextViews(subView);
+		}
+	}
+
+	private static void ClearAllTitles(UIViewController viewController)
+	{
+		foreach (var subController in viewController.ChildViewControllers)
+		{
+			if (subController.NavigationItem != null)
+			{
+				subController.NavigationItem.Title = "";
+			}
+
+			ClearAllTitles(subController);
+		}
+	}
+}
+
+
+#endif
---
+++ LaunchImageExample/Screens/DetailViewController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace LaunchImageExample
+{
+	public partial class DetailViewController : UIViewController
+	{
+		UIPopoverController masterPopoverController;
+		object detailItem;
+
+		public DetailViewController ()
+			: base (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone ? "DetailViewController_iPhone" : "DetailViewController_iPad", null)
+		{
+			Title = NSBundle.MainBundle.LocalizedString ("Detail", "Detail");
+
+			// Custom initialization
+		}
+
+		public void SetDetailItem (object newDetailItem)
+		{
+			if (detailItem != newDetailItem) {
+				detailItem = newDetailItem;
+			}
+			
+			if (masterPopoverController != null)
+				masterPopoverController.Dismiss (true);
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+		}
+
+		[Export ("splitViewController:willHideViewController:withBarButtonItem:forPopoverController:")]
+		public void WillHideViewController (UISplitViewController splitController, UIViewController viewController, UIBarButtonItem barButtonItem, UIPopoverController popoverController)
+		{
+			barButtonItem.Title = NSBundle.MainBundle.LocalizedString ("Master", "Master");
+			NavigationItem.SetLeftBarButtonItem (barButtonItem, true);
+			masterPopoverController = popoverController;
+		}
+
+		[Export ("splitViewController:willShowViewController:invalidatingBarButtonItem:")]
+		public void WillShowViewController (UISplitViewController svc, UIViewController vc, UIBarButtonItem button)
+		{
+			// Called when the view is shown again in the split view, invalidating the button and popover controller.
+			NavigationItem.SetLeftBarButtonItem (null, true);
+			masterPopoverController = null;
+		}
+	}
+}
+
